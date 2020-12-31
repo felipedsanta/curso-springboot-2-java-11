464fc90e3a999a68e1cde54d042c062c345c8b02
felipedsanta <felipe.carro3@Outlook.com>
2020-12-30 22:27:58 -0300

    Service layer, component registration

--- src/main/java/com/felps/workshop/repositories/UserRepository.java
+++ src/main/java/com/felps/workshop/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.felps.workshop.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
---
+++ src/main/java/com/felps/workshop/services/UserService.java
@@ -0,0 +1,25 @@
+package com.felps.workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felps.workshop.entities.User;
+import com.felps.workshop.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
